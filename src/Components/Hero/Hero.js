191b4aa2bf99768a8ce65b1cd2653435bdca83ff
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2022-02-05 19:37:04 -0500

    rout to parallax on hero updated for build

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import './styles.css'
-import HikerParallax from '../../Routes/FeaturedParallaxPage/HikerParallax'
+import HikerParallax from '../../Routes/FeaturedParallaxPage/ParallaxCollection/HikerParallax/index'
 // import videoFeature from './images/forestColors.mp4'
 // const videoFeature = 'https://firebasestorage.googleapis.com/v0/b/cssforge.appspot.com/o/waves.mp4?alt=media&token=95ec6278-3199-40b3-a1d2-6ae3187ad84f'
 
