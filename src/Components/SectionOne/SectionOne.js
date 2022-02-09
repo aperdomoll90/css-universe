bf4e51eeafc069e3a3749b12b5d547a4b0d69ad4
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2022-02-08 21:46:29 -0500

    twinkling stars animation added, svg planed created and size updating by props

--- src/Components/SectionOne/SectionOne.js
+++ src/Components/SectionOne/SectionOne.js
@@ -1,27 +1,16 @@
 import './styles.css'
-import Narwhal from '../../Routes/FeaturedCreaturesPage/PettingZoo/Narwhal/Narwhal'
-import Terrarium from '../../Routes/FeaturedCreaturesPage/PettingZoo/Terrarium/Terrarium'
-import FaeLoader from '../../Routes/FeaturedLoadersPage/Loaders/FaeLoader'
-import SkillBar from '../../Routes/FeaturedFormsPage/FormsAndGraphics/SkillBar'
+import CosmicChart from '../../Routes/FeaturedNavigationBarsPage/NavBars/cosmicChart'
+import PlanetCard from '../../Routes/FeaturedCreaturesPage/PettingZoo/planetCard'
+import StarrySky from '../../Routes/FeaturedCreaturesPage/PettingZoo/starySky'
 
 function SectionOne() {
   return (
     <section className='sectionOne'>
       <div className='shadow' />
-      <div className='navigationChart'>
-        <div className='firstCircle dashedChart'>
-          <div className='planetHalo dashedChart'>
-            <div className='sun' />
-          </div>
+      <CosmicChart/>
+      <PlanetCard/>
+      <StarrySky/>
 
-          <div className='secondRing dashedChart'>
-            <div className='planetHalo dashedChart'>
-              <div className='sun' />
-            </div>
-            <div className='sun' />
-          </div>
-        </div>
-      </div>
     </section>
   )
 }
