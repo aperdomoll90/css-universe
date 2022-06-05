dc4735f7fe971a759b563ba1a24b559adb83f152
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2022-06-05 17:37:10 -0400

    3d solar system created

--- src/Routes/Featured3DCSSPage/3dCollection/Universe3D/index.js
+++ src/Routes/Featured3DCSSPage/3dCollection/Universe3D/index.js
@@ -1,13 +1,26 @@
 import './styles.css'
 import { useEffect } from 'react'
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import background from './media/space.jpeg'
+import dots from './media/dots.png'
+
+import sunImage from './media/sun.jpeg'
+import mercuryImage from './media/mercury.jpeg'
+import venusImage from './media/venus.jpeg'
+import earthImage from './media/earthImage.jpeg'
 import moonImage from './media/moon.jpeg'
 import moonTextureImage from './media/normal.jpeg'
-import background from './media/space.jpeg'
+import marsImage from './media/mars.jpeg'
+import jupiterImage from './media/jupiter.jpeg'
+import saturnImage from './media/saturn.jpeg'
+import saturnRingImage from './media/saturnRing.png'
+import uranusImage from './media/uranus.jpeg'
+import uranusRingImage from './media/uranusRing.png'
 import neptuneImage from './media/neptune.jpeg'
-import dots from './media/dots.png'
+import plutoImage from './media/pluto.jpeg'
+
 // import Astronaut from './media/astronaut/Astronaut'
 
 function Universe3D() {
@@ -21,7 +34,7 @@ function Universe3D() {
 
     // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Camera <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     // camera tracks and initial position
-    const cameraOffset = new THREE.Vector3(-.5, 4, 9)
+    const cameraOffset = new THREE.Vector3(-0.5, 4, 9)
     const cameraTrack = new THREE.CubicBezierCurve3(new THREE.Vector3(0, 10, 100), new THREE.Vector3(-20, 0, 100), new THREE.Vector3(15, 0, 80), new THREE.Vector3(-5, -1, 0))
 
     // show tracks
@@ -32,22 +45,26 @@ function Universe3D() {
     // scene.add(line)
 
     // create camera
-    const fov = 60
+    const fov = 45
     const aspect = window.innerWidth / window.innerHeight
-    const near = 1.0
-    const far = 50.0
+    const near = 0.1
+    const far = 1000
     const perspectiveCamera = new THREE.PerspectiveCamera(fov, aspect, near, far)
     const camera = perspectiveCamera
-    camera.position.set(0, 15, 95).add(cameraOffset)
+    camera.position.set(-90, 140, 140);
+    // camera.position.set(0, 15, 95).add(cameraOffset)
     scene.add(perspectiveCamera)
 
-  
+    // const cameraTargetGeometry = new THREE.SphereGeometry(1, 24, 24)
+    // const cameraTargetImage = new THREE.TextureLoader().load(dots)
+    // const cameraTarget = new THREE.Mesh(cameraTargetGeometry, new THREE.MeshBasicMaterial({ map: cameraTargetImage, transparent: true }))
 
-    const cameraTargetGeometry = new THREE.SphereGeometry(2, 24, 24)
-    const cameraTargetImage = new THREE.MeshStandardMaterial({ color: 0xff6347 })
-    const cameraTarget = new THREE.Mesh(cameraTargetGeometry, cameraTargetImage)
-    cameraTarget.position.set(0, 15, 95)
-    scene.add(cameraTarget)
+    // const cameraTargetOuterGeometry = new THREE.SphereGeometry(1.5, 24, 24)
+    // const cameraTargetOuter = new THREE.Mesh(cameraTargetOuterGeometry, new THREE.MeshBasicMaterial({ map: cameraTargetImage, transparent: true }))
+
+    // cameraTarget.position.set(0, 15, 95)
+    // cameraTargetOuter.position.set(0, 15, 95)
+    // scene.add(cameraTarget,cameraTargetOuter)
 
     // const cameraHelperGeometry = new THREE.SphereGeometry(2, 24, 24)
     // const cameraHelperImage = new THREE.MeshStandardMaterial({ color: 0xff6347 })
@@ -57,9 +74,10 @@ function Universe3D() {
 
     // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Lights <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    const pointLight = new THREE.PointLight(0xffffff)
-    const ambientLight = new THREE.AmbientLight(0xffffff)
-    pointLight.position.set(30, 30, 30)
+    const pointLight = new THREE.PointLight(0xffffff, 2, 300)
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+    // scene.add(pointLight)
+    pointLight.position.set(0, 0, 0)
     scene.add(pointLight, ambientLight)
     // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> helper tools <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -87,35 +105,84 @@ function Universe3D() {
     Array(500).fill().forEach(addStar)
 
     // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Objects <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    const moonGeometry = new THREE.SphereGeometry(4, 64, 32)
+
+    const sunGeo = new THREE.SphereGeometry(16, 30, 30)
+    const sunSurface = new THREE.TextureLoader().load(sunImage)
+    const sunMat = new THREE.MeshBasicMaterial({ map: sunSurface })
+    const sun = new THREE.Mesh(sunGeo, sunMat)
+    scene.add(sun)
+
+    function createPlanet(size, texture, position, ring) {
+      // create planet
+      const geo = new THREE.SphereGeometry(size, 30, 30)
+      const surfaceImage = new THREE.TextureLoader().load(texture)
+      const mat = new THREE.MeshStandardMaterial({ map: surfaceImage })
+      const mesh = new THREE.Mesh(geo, mat)
+
+      const obj = new THREE.Object3D()
+      obj.add(mesh)
+
+      if (ring) {
+        const ringGeo = new THREE.RingGeometry(ring.innerRadius, ring.outerRadius, 32)
+        const ringSurface = new THREE.TextureLoader().load(ring.texture)
+        const ringMat = new THREE.MeshBasicMaterial({
+          map: ringSurface,
+          side: THREE.DoubleSide,
+        })
+        const ringMesh = new THREE.Mesh(ringGeo, ringMat)
+        obj.add(ringMesh)
+        ringMesh.position.x = position
+        ringMesh.rotation.x = -0.5 * Math.PI
+      }
+
+      // >>>>>>>>>>>>>  create orbit
+      const orbitGeometry = new THREE.RingGeometry(position, position + .04, 62)
+      orbitGeometry.rotateX(1.5)
+      const orbitMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide })
+      const orbit = new THREE.Mesh(orbitGeometry, orbitMaterial)
+      sun.add(orbit)
+      console.log('orbit', orbit)
+
+      scene.add(obj)
+      mesh.position.x = position
+      return { mesh, obj }
+    }
+    const mercury = createPlanet(3.2, mercuryImage, 28)
+    const venus = createPlanet(5.8, venusImage, 44)
+    const earth = createPlanet(6, earthImage, 62)
+    const mars = createPlanet(4, marsImage, 78)
+    const jupiter = createPlanet(12, jupiterImage, 100)
+    const saturn = createPlanet(10, saturnImage, 138, {
+      innerRadius: 10,
+      outerRadius: 20,
+      texture: saturnRingImage,
+    })
+    const uranus = createPlanet(7, uranusImage, 176, {
+      innerRadius: 7,
+      outerRadius: 12,
+      texture: uranusRingImage,
+    })
+    const neptune = createPlanet(7, neptuneImage, 200)
+    const pluto = createPlanet(2.8, plutoImage, 216)
+
+    const moonGeometry = new THREE.SphereGeometry(0.5, 64, 32)
     const moonSurfaceImage = new THREE.TextureLoader().load(moonImage)
     const moonTexture = new THREE.TextureLoader().load(moonTextureImage)
     const moonSurface = new THREE.MeshStandardMaterial({ map: moonSurfaceImage, normalMap: moonTexture })
     const moon = new THREE.Mesh(moonGeometry, moonSurface)
-    moon.position.setX(-15)
-    moon.position.setZ(60)
-    scene.add(moon)
+    moon.position.setX(2.8)
 
-    const neptuneGeometry = new THREE.SphereGeometry(8, 64, 32)
-    const neptuneSurfaceImage = new THREE.TextureLoader().load(neptuneImage)
-    const neptuneTexture = new THREE.TextureLoader().load(moonTextureImage)
-    const neptuneSurface = new THREE.MeshStandardMaterial({ map: neptuneSurfaceImage, normalMap: neptuneTexture })
-    const neptune = new THREE.Mesh(neptuneGeometry, neptuneSurface)
-    neptune.position.setX(-15)
-    neptune.position.setZ(15)
-    scene.add(neptune)
-
-    const dustPlanetGeometry = new THREE.SphereGeometry(5, 24, 24)
-    const dustPlanetImage = new THREE.TextureLoader().load(dots)
-    const dustPlanet = new THREE.Mesh(dustPlanetGeometry, new THREE.MeshBasicMaterial({ map: dustPlanetImage, transparent: true }))
-    dustPlanet.position.setX(20)
-    dustPlanet.position.setZ(40)
-    scene.add(dustPlanet)
+    // const dustPlanetGeometry = new THREE.SphereGeometry(5, 24, 24)
+    // const dustPlanetImage = new THREE.TextureLoader().load(dots)
+    // const dustPlanet = new THREE.Mesh(dustPlanetGeometry, new THREE.MeshBasicMaterial({ map: dustPlanetImage, transparent: true }))
+    // dustPlanet.position.setX(20)
+    // dustPlanet.position.setZ(40)
+    // scene.add(dustPlanet)
 
     // function addRing() {
-    //   const ringGeometry = new THREE.RingGeometry(6.1, 6, 200)
     //   ringGeometry.rotateY(2)
     //   ringGeometry.rotateX(2)
+    // const ringGeometry = new THREE.RingGeometry(6.1, 6, 200)
     //   const ringMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide })
     //   const ring = new THREE.Mesh(ringGeometry, ringMaterial)
     //   const [x, y, z] = Array(3)
@@ -134,9 +201,27 @@ function Universe3D() {
     // >>>>>>>>>>>>>>>>>>>>>>>>> To Render and Animate <<<<<<<<<<<<<<<<<<<<
     function animate() {
       requestAnimationFrame(animate)
-      dustPlanet.rotation.y -= 0.001
-      moon.rotation.y += 0.003
-      neptune.rotation.y += 0.003
+      // sun.rotateY(0.004)
+      mercury.mesh.rotateY(0.004)
+      venus.mesh.rotateY(0.002)
+      earth.mesh.rotateY(0.02)
+      mars.mesh.rotateY(0.018)
+      jupiter.mesh.rotateY(0.04)
+      saturn.mesh.rotateY(0.038)
+      uranus.mesh.rotateY(0.03)
+      neptune.mesh.rotateY(0.032)
+      pluto.mesh.rotateY(0.008)
+
+      mercury.obj.rotateY(0.04)
+      venus.obj.rotateY(0.015)
+      earth.obj.rotateY(0.01)
+      mars.obj.rotateY(0.008)
+      jupiter.obj.rotateY(0.002)
+      saturn.obj.rotateY(0.0009)
+      uranus.obj.rotateY(0.0004)
+      neptune.obj.rotateY(0.0001)
+      pluto.obj.rotateY(0.00007)
+
       controls.update()
       renderer.render(scene, camera)
     }
@@ -145,37 +230,64 @@ function Universe3D() {
     function moveCamera() {
       const t = window.pageYOffset
       // const t = document.body.getBoundingClientRect().top
-      // neptune.rotation.z += 0.01
-      
-      const cameraPos = cameraTrack.getPoint(t * 0.00038)      
+      // cameraTarget.rotation.y += 0.02
 
-      cameraTarget.position.set(cameraPos.x, cameraPos.y, cameraPos.z)
+      // cameraTargetOuter.rotation.z += 0.02
+      // cameraTargetOuter.rotation.x += 0.02
+
+      const cameraPos = cameraTrack.getPoint(t * 0.00038)
+
+      // cameraTarget.position.set(cameraPos.x, cameraPos.y, cameraPos.z)
+      // cameraTargetOuter.position.set(cameraPos.x, cameraPos.y, cameraPos.z)
       // cameraHelper.position.set(cameraPos.x, cameraPos.y, cameraPos.z).add(cameraOffset)
 
       camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z).add(cameraOffset)
     }
     document.body.onscroll = moveCamera
+
+    // // >>>>>>> Menu Animation
+    // let sectionSpace = document.querySelectorAll('.sectionSpace')
+
+    // const sectionObserver = new IntersectionObserver(
+    //   entries => {
+    //     entries.forEach(entry => {
+    //       entry.target.classList.toggle('sectionAnimationOpacity', entry.isIntersecting)
+    //       // if (entry.isIntersecting) sectionObserver.unobserve(entry.target)
+    //     })
+    //   },
+    //   {
+    //     threshold: 0.8,
+    //     // rootMargin: '30%',
+    //   }
+    // )
+
+    // sectionSpace.forEach(card => {
+    //   sectionObserver.observe(card)
+    // })
+    window.addEventListener('resize', function () {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+    })
   }, [])
 
   return (
     <div id='universe3D-wrapper'>
-      <canvas id='bg'>
-        {/* <Astronaut/> */}
-      </canvas>
-      <div className='space-scroll-content'>
+      <canvas id='bg'>{/* <Astronaut/> */}</canvas>
+      {/* <div className='space-scroll-content'>
         <section>
           <p>Section 1</p>
         </section>
-        <section>
+        <section className='sectionSpace'>
           <p>Section 2</p>
         </section>
         <section>
           <p>Section 3</p>
         </section>
-        <section>
+        <section className='sectionSpace'>
           <p>Section 4</p>
         </section>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/Routes/Landing/index.js
+++ src/Routes/Landing/index.js
@@ -1,15 +1,89 @@
-import '../../App.css'
-import Hero from '../../Components/Hero/Hero'
-import SectionOne from '../../Components/SectionOne/SectionOne'
+import { useEffect } from 'react'
+import './styles.css'
 
+import SolarSystem from '../../Components/SolarSystem'
 
+const hiker = 'https://firebasestorage.googleapis.com/v0/b/cssforge.appspot.com/o/hiker%20parallax%2Fhiker.png?alt=media&token=f5c535a2-55a9-413a-a0cd-852885d45205'
+const plane2 = 'https://firebasestorage.googleapis.com/v0/b/cssforge.appspot.com/o/hiker%20parallax%2Fplane2.png?alt=media&token=7ddaf0a7-fd3b-4d72-a745-fd82bbf506d9'
+const plane3 = 'https://firebasestorage.googleapis.com/v0/b/cssforge.appspot.com/o/hiker%20parallax%2Fplane3.png?alt=media&token=9e7bc464-5270-4e28-a4fb-b08e67d65359'
+const backgroundMountain = 'https://firebasestorage.googleapis.com/v0/b/cssforge.appspot.com/o/hiker%20parallax%2Fplane4.png?alt=media&token=4d2c89ac-dfac-469b-a272-add271a7ac0f'
 
 function Landing() {
-  
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      let scroll = window.pageYOffset
+
+      const hiker = document.querySelector('.hiker')
+      const plane2 = document.querySelector('.plane2')
+      const plane3 = document.querySelector('.plane3')
+      const backgroundMountain = document.querySelector('.backgroundMountain')
+      const shadow = document.querySelector('.landing-bottom-shadow')
+      const indicator = document.querySelector('.landing-scroll-indicator')
+
+      //********* GET the height of the Container Section
+      const header = document.querySelector('.landing-Parallax') // ***(Selects Section that Contains the component)
+     if (header){
+      let header_height = header.offsetHeight //***( Finds the Height Value of set Section)
+         //****** Alter opacity
+         shadow && (shadow.style.opacity = -scroll / (header_height / 2) + 1)
+         indicator && (indicator.style.opacity = -scroll / (header_height / 2) + 1)
+     }
+     
+
+      // >>>>>>>>>>>>>>>>>>> TRANSLATE X and or Y
+      hiker.style.transform = `translateY(${scroll * 1.5}px)`
+      plane2.style.transform = `translateX(${scroll * 0.4}px) translateY(${scroll * 1}px)`
+      plane3.style.transform = `translateX(${scroll * -0.4}px) translateY(${scroll * 1}px)`
+      backgroundMountain.style.transform = `translateX(${scroll * -1.5}px) translateY(${scroll * 0.8}px)`
+
+   
+      
+      // // >>>>>>> Menu Animation
+
+    const sectionSpace = document.querySelectorAll('.content-section')
+
+    const sectionObserver = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          entry.target.classList.toggle('sectionAnimationOpacity', entry.isIntersecting)
+          // if (entry.isIntersecting) sectionObserver.unobserve(entry.target)
+        })
+      },
+      {
+        threshold: 1,
+        // rootMargin: '5%',
+      }
+    )
+
+    sectionSpace.forEach(card => {
+      sectionObserver.observe(card)
+    })
+    
+    })
+  }, [])
   return (
-    <div className='App'>
-      <Hero />
-      <SectionOne />
+    <div className='route-wrapper'>
+      <div className='landing-Parallax'>
+        <img src={hiker} className='hiker contentOnLoadAnimation' data-speed='1.5' alt='hiker' />
+        <img src={plane2} className='plane2  contentOnLoadAnimation' data-speed='1.1' alt='mountain forest' />
+        <img src={plane3} className='plane3  contentOnLoadAnimation' data-speed='0.67' alt='medium mountain' />
+        <img src={backgroundMountain} className='backgroundMountain  contentOnLoadAnimation' alt='High mountain peeks covered in snow' />
+        <p className='landing-scroll-indicator'>SCROLL</p>
+        <div className='landing-bottom-shadow' />
+        <SolarSystem />
+      </div>
+
+      <section className='content-section'>
+        <p className='site-intro-paragraph'>
+          CSS Forge is a place to showcase my personal UI designs, CSS tricks and animations.
+          <br />
+          <br />
+          CSS forge a GIF FREE ZONE if is not a picture is code.
+          <br />
+          <br />
+          Hope you enjoy them, as much as I enjoyed creating them!
+        </p>
+      </section>
     </div>
   )
 }
